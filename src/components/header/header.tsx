6660255ccacf04be2980d46905251dd7a6fd73be
Noam Malka <noamalka2903@gmail.com>
2026-04-13 18:47:53 +0300

    Added comment

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -64,6 +64,7 @@ export default function Header() {
           )}
 
           {/* Auth buttons */}
+          {/* TODO: Remove the option to change/remove/add email address */}
           <SignedOut>
             <Button
               variant="contained"
